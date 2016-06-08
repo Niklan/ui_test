5e8002b8b99ad3053eec1940e6c0d2a9b999f005
Niklan <niklanrus@gmail.com>
2016-06-08 17:43:25 +0500

    Added few examples for starting point and edidted README.

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ This module provide page with maximum UI elements for testing themes.
 Created for testing [Glisseo](https://github.com/Niklan/Glisseo) base theme, but there is no dependency, most of features
 are universal.
 
+Inspired of demo content pages frameworks like Bootstrap, Semantic UI and others, this can help you testing many of
+things in theme. You can easily edit plain HTML code for your purposes and your theme idea. This is just boilerplate
+for HTML and CSS with some Drupal specific staff.
 
 License GPL v2.
--- theme/ui-test-page.tpl.php
+++ theme/ui-test-page.tpl.php
@@ -1,1 +1,52 @@
-test
+<h2>Table of content</h2>
+<ul>
+  <li><a href="#headings">Headings</a></li>
+</ul>
+
+<!-- Headings -->
+<h2 id="headings">Headings</h2>
+<div class="example">
+  <h1>h1. Testing heading</h1>
+  <h2>h2. Testing heading</h2>
+  <h3>h3. Testing heading</h3>
+  <h4>h4. Testing heading</h4>
+  <h5>h5. Testing heading</h5>
+  <h6>h6. Testing heading</h6>
+</div>
+
+<h2>Headers with secondary text.</h2>
+<div class="example">
+  <h1>h1. Testing heading
+    <small>Secondary text</small>
+  </h1>
+  <h2>h2. Testing heading
+    <small>Secondary text</small>
+  </h2>
+  <h3>h3. Testing heading
+    <small>Secondary text</small>
+  </h3>
+  <h4>h4. Testing heading
+    <small>Secondary text</small>
+  </h4>
+  <h5>h5. Testing heading
+    <small>Secondary text</small>
+  </h5>
+  <h6>h6. Testing heading
+    <small>Secondary text</small>
+  </h6>
+</div>
+
+<h2>Body paragraphs</h2>
+<div class="example">
+  <p>Generosity communities initiative; gender rights, human being local global
+    network. Non-partisan many voices, community health workers collaborative
+    cities insurmountable challenges innovation contribution sustainability
+    catalytic effect. </p>
+  <p>Democracy fundraising campaign research United Nations medicine metrics
+    freedom health, inspiration 501(c)(3), meaningful work Cesar Chavez
+    progressive. Medical world problem solving lifting people up our grantees
+    and partners change-makers healthcare donate lasting change.</p>
+  <p> Resourceful public sector, protect; marginalized communities, humanitarian
+    synthesize equality public-private partnerships advocate effect Nelson
+    Mandela employment Andrew Carnegie.</p>
+</div>
